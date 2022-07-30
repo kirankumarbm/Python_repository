fcd11ac7bc1635e0b714fc4d1cd1d19bacab442f
kirankumarbm <itskirankumar98@gmail.com>
2022-07-30 14:36:00 +0530

    Sorting Algorithm

---
+++ Quick_sort.py
@@ -0,0 +1,10 @@
+class Quick:
+
+    def quick(self):
+        n= int(input("Enter Size of Array: "))
+        list =[]
+        for i in range(n):
+            ele = int(input("Enter Element: "))
+            list.append()
+        print(list)
+a = Quick()
--- Sorting_Insert_Selection_Bubble.py
+++ Sorting_Insert_Selection_Bubble.py
@@ -1,4 +1,86 @@
-# Insertion Sorting, Selection Sorting , Bubble Sorting , Merge Sorting and Quick Sorting
+# Insertion Sorting, Selection Sorting , Bubble Sorting
+import sys
+class Sorting:
+    n = int(input("Enter Length of the Array "))
+    list = []
+    for i in range(n):
+        ele = int(input("Enter Element: "))
+        list.append(ele)
+
+    @classmethod
+    def bubble_sorting(cls):
+        ll = []
+        ll.extend(cls.list)       # coping static l[] ---> ll[]
+        m = cls.n              # coping static n--> m
+        # below is Logic
+        for i in range(m-1):
+            for j in range(m-1-i):
+                if ll[j] > ll[j+1]:
+                    ll[j], ll[j+1] = ll[j+1], ll[j]
+        # print Result
+        print("\nBubble Sort Result : ", end="")
+        for i in ll:
+            print(i, end=" ")
+        print("")
+
+    @classmethod
+    def selection_sorting(cls):
+        ll = []
+        ll.extend(cls.list)  # coping static l[] ---> ll[]
+        m = cls.n  # coping static n--> m
+        # below is Logic
+        for i in range(m-1):
+            min = i
+            j = i+1
+            while j< m :
+                if ll[j] < ll[min]:
+                    min = j
+                j = j + 1
+            ll[min], ll[i] = ll[i], ll[min]
+        # print Result
+        print("\nSelection Sort Result : ", end="")
+        for i in ll:
+            print(i, end=" ")
+        print("")
+
+    @classmethod
+    def insertion_sorting(cls):
+        ll = []
+        ll.extend(cls.list)  # coping static l[] ---> ll[]
+        m = cls.n  # coping static n--> m
+        # below is Logic
+        i = 1
+        while i <= m-1:
+            key = ll[i]
+            j = i-1
+            while j >=0 and ll[j] > key:
+                ll[j+1] = ll[j]
+                j = j - 1
+            ll[j+1] = key
+            i = i + 1
+        # Print Result
+        print("\nInsertion Sort Result : ", end="")
+        for i in ll:
+            print(i, end=" ")
+        print("")
+
+
+while True:
+    print("\n......Different Sorting Technique.....")
+    print("b - Bubble Sorting\ts - Selection Sorting\ti - Insertion Sorting"
+          "\ne - Exit\n............")
+    option = input("Choose Your Option : ")
+    if option.lower() == 'b':
+        Sorting.bubble_sorting()
+    elif option.lower() == 's':
+        Sorting.selection_sorting()
+    elif option.lower() == 'i':
+        Sorting.insertion_sorting()
+    elif option.lower() == 'e':
+        sys.exit()
+    else:
+        print("invalid Entry.........Enter valid Option ")
+
 
 
 
